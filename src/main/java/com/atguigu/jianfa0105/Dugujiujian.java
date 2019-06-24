0999faf4838b3e82f92403589091fd7fef16ee87
cst1314 <1483037714@qq.com>
2019-06-24 23:58:55 +0800

    huashan jianfa 1

--- src/main/java/com/atguigu/jianfa0105/Dugujiujian.java
+++ src/main/java/com/atguigu/jianfa0105/Dugujiujian.java
@@ -8,6 +8,6 @@ package com.atguigu.jianfa0105;
  */
 public class Dugujiujian {
     public static void main(String[] args) {
-        System.out.println("hello  dugujiujian!!!");
+        System.out.println("hello  dugujiujian123123!!!");
     }
 }
