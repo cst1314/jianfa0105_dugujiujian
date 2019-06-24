4e01eeb8a4e540f06af8270f02b3872d414d9fb6
cst1314 <1483037714@qq.com>
2019-06-25 00:44:57 +0800

    new jianfa

---
+++ src/main/java/com/atguigu/jianfa0105/NewJianfa.java
@@ -0,0 +1,13 @@
+package com.atguigu.jianfa0105;
+
+/**
+ * 描述:
+ *
+ * @author caoshuitao
+ * @create 2019-06-25
+ */
+public class NewJianfa {
+    public static void main(String[] args) {
+        System.out.println("new jianfa!!!!!!!!!");
+    }
+}
